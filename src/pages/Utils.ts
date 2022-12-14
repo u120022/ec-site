228116c6a909c065b09542d0c3ca4cdc53c8d9a8
u120022 <u120022@st.pu-toyama.ac.jp>
2022-12-14 06:45:51 +0000

    Refactor Util.ts

--- src/pages/Utils.ts
+++ src/pages/Utils.ts
@@ -1,6 +1,7 @@
 import { useParams, useSearchParams } from "@solidjs/router";
 
-export const useParamInt = <T>(key: string, initValue: T) => {
+// urlパターンを解析して抽出
+export function useParamInt<T>(key: string, initValue: T) {
   const params = useParams();
 
   const getParam = () => {
@@ -13,9 +14,10 @@ export const useParamInt = <T>(key: string, initValue: T) => {
   };
 
   return getParam;
-};
+}
 
-export const useSearchParam = <T>(key: string, initValue: T) => {
+// getクエリを抽出
+export function useSearchParam<T>(key: string, initValue: T) {
   const [params, setParams] = useSearchParams();
 
   const getParam = () => {
@@ -28,9 +30,10 @@ export const useSearchParam = <T>(key: string, initValue: T) => {
   };
 
   return [getParam, setParam] as [() => string | T, (value: string) => void];
-};
+}
 
-export const useSearchParamInt = <T>(key: string, initValue: T) => {
+// getクエリを解析して抽出
+export function useSearchParamInt<T>(key: string, initValue: T) {
   const [params, setParams] = useSearchParams();
 
   const getParam = () => {
@@ -47,12 +50,13 @@ export const useSearchParamInt = <T>(key: string, initValue: T) => {
   };
 
   return [getParam, setParam] as [() => number | T, (value: number) => void];
-};
+}
 
-export const calcMaxPageCount = (
+// 最大ページを計算
+export function calcMaxPageCount(
   count: number | undefined,
   countPerPage: number
-) => {
+) {
   if (count == undefined) return 1;
   return Math.ceil(count / countPerPage);
-};
+}
